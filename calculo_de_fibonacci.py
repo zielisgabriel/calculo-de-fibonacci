a40919da5b0e17c86c06f4b3c11d0226e4db3bf6
Gabriel <josegabrielas0586@gmail.com>
2024-09-08 14:04:57 -0300

    Pequenas alterações

--- calculo_de_fibonacci.py
+++ calculo_de_fibonacci.py
@@ -3,13 +3,16 @@ fibonacci = []
 x = 0
 y = 1
 
-z = int(input('Digite um valor de repetição: '))
+num = int(input('Digite um número: '))
 
-for _ in range(z):
+for _ in range(num):
     fibonacci.append(x)
     fibonacci.append(y)
     soma = x + y
     x = soma
     y = soma + y
 
-print(fibonacci)
+if (num in fibonacci):
+    print(f'O número {num} está na sequência de Fibonacci')
+else:
+    print(f'O número {num} não está na sequência de Fibonacci')
